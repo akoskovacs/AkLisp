fc62052aae9d8220b1a474c2caf8a7e0376153fa
Ákos Kovács <akoskovacs@gmx.com>
2012-10-31 03:46:59 +0100

    modules/hello.c: To verbose explainations removed

--- modules/hello.c
+++ modules/hello.c
@@ -1,17 +1,6 @@
 /* This is a demonstration of the AkLisp module capabilities. */
-/* It can be loaded up by (LOAD "modules/hello.alm") */
-/* You must always include this header (located under ../src/) */
 #include <aklisp.h>
 #include <stdio.h>
-
-/* Modules can define simple functions with AKL_CFUN_DEFINE()
-   It has the following arguments:
-        o Function name (as in C)
-        o A variable for the environment,
-            with the type 'akl_instance' (mostly 'in')
-        o A variable for the list of it's arguments, with the type
-            of 'akl_list' (mostly 'args')
-*/ 
 
 AKL_CFUN_DEFINE(hello, in, args)
 {
@@ -23,36 +12,21 @@ AKL_CFUN_DEFINE(hello, in, args)
 
 
 /* Every module must have two functions with this signature */
+/* One for initializing and one for destroying resources. */
 /* Each of them should return 0 when everything is good */
 static int hello_load(struct akl_instance *in)
 {
-   /* You can add functions to the environment with AKL_ADD_CFUN()
-      It has the following arguments:
-        o Instance of the interpreter
-        o Name of the function (as in AKL_CFUN_DEFINE)
-        o Name of the function in the interpreter
-        o Description of the function */
    AKL_ADD_CFUN(in, hello, "HELLO", "Hey hello!"); 
    return AKL_LOAD_OK;
 }
 
-/* One for initializing and one for destroying resources. */
 static int hello_unload(struct akl_instance *in)
 {
-   /* Every resource must free()'d up with AKL_REMOVE_CFUN */
    AKL_REMOVE_CFUN(in, hello);
    return AKL_LOAD_OK;
 }
 
-/* Every module must have a AKL_MODULE_DEFINE() in it.
-   The first three parameters must be given, all other can be NULL
-   It has the following arguments:
-        o Loader code (code pointer)
-        o Unloader code
-        o Module name
-        o Module description
-        o Author
-*/
 AKL_MODULE_DEFINE(hello_load, hello_unload, "hello"
     , "A simple demo module", "Akos Kovacs");
 
+/* No need for further explaination, the code explains itself... */
