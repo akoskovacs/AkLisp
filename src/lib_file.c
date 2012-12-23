f38f7dd8da0bc354883f97ff23d4cf8411f26f68
Ákos Kovács <akoskovacs@gmx.com>
2012-12-23 08:46:38 +0100

    lib_file.c: Must aklisp.h include must be local

--- src/lib_file.c
+++ src/lib_file.c
@@ -20,8 +20,8 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  ************************************************************************/
+#include "aklisp.h"
 #include <stdio.h>
-#include <aklisp.h>
 
 /* Our current type id */
 static akl_utype_t akl_file_utype;
