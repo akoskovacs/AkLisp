77f9292587401c7076a1e08cafce8dcb2dcdd900
Ákos Kovács <akoskovacs@gmx.com>
2013-01-05 23:56:12 +0100

    lib_spec.c: New file for special forms
        o All special forms will be called by the parser
        o These forms must parse the input manually

--- src/lib_spec.c
+++ src/lib_spec.c
@@ -22,40 +22,95 @@
  ************************************************************************/
 #include "aklisp.h"
 
+/*
+ * These functions are special forms. They build their own internal
+ * representation, by parsing the tokens.
+*/
 #define AKL_SPEC_DEFINE(name, state, args, instr) \
-    void akl_spec_##name(struct akl_state * state \
-    , struct akl_list * args, struct akl_vector * instr)
+    static void akl_spec_##name(struct akl_state * state \
+    , struct akl_vector * ir , struct akl_io_device * dev)
 
-AKL_SPEC_DEFINE(gt, s, args, instr) {
-    struct akl_instruction cin;
-    struct akl_value *a[2];
 
-    a[0] = AKL_FIRST_VALUE(args);
-    a[1] = AKL_SECOND_VALUE(args);
+AKL_SPEC_DEFINE(if, s, ir, dev)
+{
+    struct akl_vector *branch;
 
-    if (!a[0] || !a[1] || (a[0]->va_type != a[1]->va_type != TYPE_NUMBER))
-        return; /* Throw error */
-
-    cin.in_operation = &akl_instructions[AKL_OP_GT_N];
-    cin.in_operands[0] = a[0];
-    cin.in_operands[1] = a[1];
-    akl_vector_push(instr, &cin);
+    /* Compile the condition (while pushing the instructions to the list) */
+    akl_compile_list(s, cl, ir);
+    /* Create the new branches */
+    tb = akl_vector_new(s, 10, sizeof(struct akl_ir_instruction));
+    fb = akl_vector_new(s, 10, sizeof(struct akl_ir_instruction));
+    /* Compile the true branch, and place it in the new vector */
+    akl_compile_list(s, dev, tb);
+    /* Compile the false branch, and place it in the new vector */
+    akl_compile_list(s, dev, fb);
+    /* Add the branch instruction to the IR */
+    akl_build_branch(ir, tb, fb);
 }
 
-AKL_SPEC_DEFINE(if, s, args, instr) {
-    struct akl_value *a[3];
-    struct akl_instruction jmp;
-    jmp.in_operation = &akl_instructions[AKL_OP_JMP];
+AKL_SPEC_DEFINE(while, s, ir, dev)
+{
+    akl_token_t tok;
+    struct akl_vector *cond = akl_vector_new(s, 2
+                             , sizeof(struct akl_ir_instruction));
 
-    a[0] = AKL_FIRST_VALUE(args);
-    a[1] = AKL_SECOND_VALUE(args);
-    a[2] = AKL_THIRD_VALUE(args);
+    if ((tok = akl_lex(dev)) != tRBRACE) {
+        return; /* Panic */
+    }
+    akl_compile_list(s, dev, cond);
+    /* TODO... */
+}
 
-    if (AKL_CHECK_TYPE(a[0], TYPE_LIST))
-        struct akl_list *cond = AKL_GET_LIST_VALUE(a[0]);
-    else
-        return; /* TODO: Throw error */
+struct akl_vector *akl_parse_params(struct akl_state *s, struct akl_vector *args
+                                    , struct akl_io_device *dev)
+{
+    akl_token_t tok;
+    char *opt_name;
+    while ((tok = akl_lex(dev)) != tRBRACE) {
+        if (tok == tATOM) {
+           opt_name = akl_lex_get_atom(dev);
+           akl_vector_push(args, &opt_name);
+        } else {
+            /* TODO: Scream! */
+        }
+    }
+    return args;
+}
 
-    akl_compile_list(s, cond);
+AKL_SPEC_DEFINE(defun, s, ir, dev)
+{
+    struct akl_function *func = akl_new_function(s);
+    struct akl_vector *fbody = akl_vector_new(s, 15
+                                  , sizeof(struct akl_ir_instruction));
+    func->fn_body = fbody;
+    akl_vector_init(&func->fn_args, 4, sizeof(char *));
+    if (akl_lex(dev) == tATOM) {
+        func->fn_name = akl_lex_get_atom(dev);
+    } else {
+        /* TODO: No name... */
+    }
+    func->fn_args = akl_parse_params(s, dev);
+    /* Eat the next left brace and interpret the body... */
+    if (akl_lex(dev) == tLBRACE){
+        akl_compile_list(s, fbody, dev);
+    } else {
+        /* TODO: Scream again! */
+    }
+}
 
+AKL_SPEC_DEFINE(lambda, s, ir, dev)
+{
+    struct akl_function *func = akl_new_function(s);
+    char *fname;
+    struct akl_vector *fbody = akl_vector_new(s, 10
+                                  , sizeof(struct akl_ir_instruction));
+    func->fn_body = fbody;
+    akl_vector_init(&func->fn_args, 4, sizeof(char *));
+    func->fn_args = akl_parse_params(s, dev);
+    /* Eat the next left brace and interpret the body... */
+    if (akl_lex(dev) == tLBRACE){
+        akl_compile_list(s, fbody, dev);
+    } else {
+        /* TODO: Scream again! */
+    }
 }
