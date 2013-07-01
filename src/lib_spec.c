060adefe6ed9bcbcef11b48430ff032b1ef33fcf
Ákos Kovács <akoskovacs@gmx.com>
2013-07-02 01:14:57 +0200

    lib_spec.c: Early special-form functions

--- src/lib_spec.c
+++ src/lib_spec.c
@@ -26,113 +26,134 @@
  * These functions are special forms. They build their own internal
  * representation, by parsing the tokens.
 */
-#define AKL_SPEC_DEFINE(name, state, args, ctx) \
-    static void akl_spec_##name(struct akl_state * state \
-    , struct akl_list * args , struct akl_context * ctx)
+#define AKL_SPEC_DEFINE(name, ctx) \
+    static void akl_spec_##name(struct akl_context * ctx)
 
-#define AKL_SPEC_DEFINE(name, state, args, instr) \
-    static void akl_spec_##name(struct akl_state * state \
-    , struct akl_vector * ir , struct akl_io_device * dev)
-
-void akl_build_label(struct akl_vector *ir, struct akl_label *l)
+void akl_build_label(struct akl_context *ctx, struct akl_label *l)
 {
-    l->ab_code = akl_vector_count(ir);
+    l->la_ir = ctx->cx_ir;
+    l->la_branch = AKL_LIST_LAST(ctx->cx_ir);
 }
 
-struct akl_label *akl_new_labels(struct akl_state *s, int n)
+struct akl_label *akl_new_labels(struct akl_context *ctx, int n)
 {
+    struct akl_ufun *fn = &ctx->cx_func->fn_body.ufun;
     struct akl_label *labels = (struct akl_label *)
-            akl_calloc(s, sizeof(struct akl_label), n);
+        akl_vector_reserve_more(fn->uf_labels, n);
+            
     return labels;
 }
 
-AKL_SPEC_DEFINE(if, s, ir, dev)
+AKL_SPEC_DEFINE(if, ctx)
 {
     /* Allocate the branch */
-    struct akl_label *label = akl_new_labels(s, 3);
+    struct akl_label *label = akl_new_labels(ctx, 3);
 
     /* Condition:*/
-    akl_compile_list(ir, dev);
-    akl_build_jump(s, AKL_JMP_TRUE, label+0);
-    akl_build_jump(s, AKL_JMP_FALSE, label+1);
+    akl_compile_list(ctx);
+    akl_build_jump(ctx, AKL_JMP_TRUE, label+0);
+    akl_build_jump(ctx, AKL_JMP_FALSE, label+1);
 
     /* .L0: True branch: */
-    akl_build_label(ir, label+0);
-    akl_compile_list(s, ir, dev);
-    akl_build_jump(s, AKL_JMP, label+2);
+    akl_build_label(ctx, label+0);
+    akl_compile_list(ctx);
+    akl_build_jump(ctx, AKL_JMP, label+2);
 
     /* .L1: False branch: */
-    akl_build_label(ir, label+1);
-    akl_compile_list(s, ir, dev, label+1);
-    akl_build_jump(s, AKL_JMP, label+2);
+    akl_build_label(ctx, label+1);
+    akl_compile_list(ctx);
+    akl_build_jump(ctx, AKL_JMP, label+2);
 
     /* .L2: Continue... */
-    akl_build_label(ir, label+2);
+    akl_build_label(ctx, label+2);
 }
 
-AKL_SPEC_DEFINE(while, s, ir, dev)
+AKL_SPEC_DEFINE(while, ctx)
 {
     akl_token_t tok;
-    struct akl_label *label = akl_new_labels(s, 2);
-    if ((tok = akl_lex(dev)) != tLBRACE) {
+    struct akl_label *label = akl_new_labels(ctx, 3);
+    if ((tok = akl_lex(ctx->cx_dev)) != tLBRACE) {
         return; /* Panic */
     }
     /* .L0: Condition: */
-    akl_build_label(ir, label+0);
-    akl_compile_list(s, dev, ir);
-    /* If the condition is false, don't need to get in the loop */
-    akl_build_jump(ir, AKL_JMP_FALSE, label+1);
-    
-    while ((tok = akl_lex(dev)) != tRBRACE) {
-        akl_compile_list(s, dev, ir);
+    akl_build_label(ctx, label+0);
+    akl_compile_list(ctx);
+    /* False condition, do not loop */
+    akl_build_jump(ctx, AKL_JMP_FALSE, label+1);
+    /* Exit from to loop */
+    akl_build_jump(ctx, AKL_JMP_TRUE, label+2);
+
+    while ((tok = akl_lex(ctx->cx_dev)) != tRBRACE) {
+        akl_compile_list(ctx);
     }
     /* Jump back to the condition: (with an unconditional) */
-    akl_build_jump(ir, AKL_JMP, label+0);
+    akl_build_jump(ctx, AKL_JMP, label+0);
 
     /* .L1: Getting out of the loop... */
-    akl_build_label(ir, label+1);
+    akl_build_label(ctx, label+2);
 }
 
-AKL_SPEC_DEFINE(cond, s, ir, dev)
-{
-
-}
-
-struct akl_vector *akl_parse_params(struct akl_state *s, struct akl_vector *args
-                                    , struct akl_io_device *dev)
+char **akl_parse_params(struct akl_state *s, struct akl_io_device *dev)
 {
     akl_token_t tok;
-    char *opt_name;
+    int i = 0;
+    const int DEF_ARGC = 3;
+    char **args = (char **)akl_calloc(s, DEF_ARGC, sizeof(char *));
+
     while ((tok = akl_lex(dev)) != tRBRACE) {
         if (tok == tATOM) {
-           opt_name = akl_lex_get_atom(dev);
-           akl_vector_push(args, &opt_name);
+           args[i] = akl_lex_get_atom(dev);
+           if (++i >= DEF_ARGC) {
+               args = akl_realloc(s, args, i+DEF_ARGC);
+           }
         } else {
-            /* TODO: Scream! */
+            /* TODO: hoho, pattern matching... */
         }
+        args[i] = NULL;
     }
     return args;
 }
 
-AKL_SPEC_DEFINE(defun, s, ir, dev)
+AKL_SPEC_DEFINE(defun, ctx)
 {
-    struct akl_function *func = akl_new_function(s);
-    struct akl_vector *fbody = akl_vector_new(s, 15
-                                  , sizeof(struct akl_ir_instruction));
-    func->fn_body = fbody;
-    akl_vector_init(&func->fn_args, 4, sizeof(char *));
-    if (akl_lex(dev) == tATOM) {
-        func->fn_name = akl_lex_get_atom(dev);
+    struct akl_ufun *ufun;
+    akl_token_t tok;
+    struct akl_function *func = akl_new_function(ctx->cx_state);
+    struct akl_value *fval = akl_new_function_value(ctx->cx_state, func);
+    struct akl_atom *fatm;
+
+    func->fn_type = AKL_FUNC_USER;
+    ufun = &func->fn_body.ufun;
+    akl_init_list(&ufun->uf_body);
+
+    if (akl_lex(ctx->cx_dev) == tATOM) {
+        fatm = akl_new_atom(ctx->cx_state, akl_lex_get_atom(ctx->cx_dev));
+        fatm->at_value = fval;
     } else {
-        /* TODO: No name... */
+        /* TODO: Error! */
     }
-    func->fn_args = akl_parse_params(s, dev);
+
+    ufun->uf_args = akl_parse_params(ctx->cx_state, ctx->cx_dev);
     /* Eat the next left brace and interpret the body... */
-    if (akl_lex(dev) == tLBRACE){
-        akl_compile_list(s, fbody, dev);
-    } else {
-        /* TODO: Scream again! */
+    tok = akl_lex(ctx->cx_dev);
+    if (tok == tSTRING) {
+        fatm->at_desc = akl_lex_get_string(ctx->cx_dev);
+        fatm->at_is_cdef = FALSE;
+        tok = akl_lex(ctx->cx_dev);
     }
+
+    if (tok == tLBRACE) {
+        akl_compile_list(ctx);
+    } else {
+        ; /* Todo: Error: no body */
+    }
+
+    akl_add_global_atom(ctx->cx_state, fatm);
+}
+
+#if 0
+AKL_SPEC_DEFINE(setq, ctx)
+{
 }
 
 AKL_SPEC_DEFINE(lambda, s, ir, dev)
@@ -151,20 +172,4 @@ AKL_SPEC_DEFINE(lambda, s, ir, dev)
         /* TODO: Scream again! */
     }
 }
-
-AKL_SPEC_DEFINE(defun, s, args, ctx)
-{
-    struct akl_value *fn, *a, *body;
-    struct akl_atom *atm;
-    fn = AKL_FIRST_VALUE(args);
-    a = AKL_SECOND_VALUE(args);
-    if (AKL_CHECK_TYPE(fn, TYPE_ATOM)) {
-        atm = AKL_GET_ATOM_VALUE(fn);
-    } else {
-        return;
-    }
-    switch (a->va_type) {
-        case TYPE_STRING:
-        atm->at_desc = AKL_GET_STRING_VALUE(a);
-    }
-}
+#endif
